aa0b6ce1fef641c96484e8a1119ce4657d3b6575
Pramakant Kodamsingh <pramakantkodamsingh93@gmail.com>
2025-07-01 00:22:12 +0530

    main.ts changes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import appConfiguration from './app.configuration';
+import { DbModule } from './db/db.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfiguration],
+    }),
+    DbModule,
+  ],  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import appConfiguration from './app.configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = appConfiguration().PORT;
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
